89f3c43c067bf4937fd42a200243dc00c778cd8e
ashwin <aswinshenoy65@gmail.com>
2019-08-02 10:06:30 +0530

    BugFix: lg sizing issue in Grid Col

--- utility/BootstrapNotation.js
+++ utility/BootstrapNotation.js
@@ -2,20 +2,18 @@ export function getRespValue(type, val) {
   if (val != null) {
     if (Array.isArray(val) && val.length <= 5) {
       if (val.length === 2) {
-        return `${type}-${val[0]}  ${type}-md-${val[1]}`;
+        return `${type}-${val[0]} ${type}-md-${val[1]}`;
       }
       if (val.length === 3) {
-        return `${type}-${val[0]}  ${type}-md-${val[1]} ${type}-xl-${val[2]}`;
+        return `${type}-${val[0]} ${type}-md-${val[1]} ${type}-xl-${val[2]}`;
       }
       if (val.length === 4) {
-        return `${type}-${val[0]}  ${type}-md-${val[1]} ${type}-lg-${
-          val[2]
-        } ${type}-xl-${val[3]}`;
+        return `${type}-${val[0]} ${type}-md-${val[1]} 
+        ${type}-lg-${val[2]} ${type}-xl-${val[3]}`;
       }
       if (val.length === 5) {
-        return `${type}-${val[0]}  ${type}-sm-${val[1]} ${type}-md-${
-          val[2]
-        } ${type}-xl-${val[3]} ${type}-xl-${val[3]}`;
+        return `${type}-${val[0]} ${type}-sm-${val[1]} ${type}-md-${val[2]} 
+        ${type}-lg-${val[3]} ${type}-xl-${val[4]}`;
       }
     } else if (val > 0 && val < 13) {
       return `${type}-${val}`;
